d12b4b5061771c2e180f566d1bdd7c5e892a2cde
Fu-Shengsheng <fushengsheng@live.com>
2019-09-08 16:57:39 +0800

    first time push for rust

---
+++ guessing/target/debug/deps/guessing-be9cbbac1ed1c251.d
@@ -0,0 +1,5 @@
+C:\Users\fushe\Desktop\Rust-1\Rust\guessing\target\debug\deps\guessing-be9cbbac1ed1c251.exe: src\main.rs
+
+C:\Users\fushe\Desktop\Rust-1\Rust\guessing\target\debug\deps\guessing-be9cbbac1ed1c251.d: src\main.rs
+
+src\main.rs:
---
+++ guessing/target/debug/guessing.d
@@ -0,0 +1,1 @@
+C:\Users\fushe\Desktop\Rust-1\Rust\guessing\target\debug\guessing.pdb: C:\Users\fushe\Desktop\Rust-1\Rust\guessing\src\main.rs
