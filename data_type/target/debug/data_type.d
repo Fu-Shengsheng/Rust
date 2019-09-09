f928b325579cf9a907be1ad29b44e03488b20806
Fu-Shengsheng <fushengsheng@live.com>
2019-09-10 00:06:50 +0800

    Integer of scalar

---
+++ data_type/target/debug/data_type.d
@@ -0,0 +1,1 @@
+C:\Users\fushe\Desktop\Rust\data_type\target\debug\data_type.pdb: C:\Users\fushe\Desktop\Rust\data_type\src\main.rs
---
+++ data_type/target/debug/deps/data_type-8bf0d6db10293559.d
@@ -0,0 +1,5 @@
+C:\Users\fushe\Desktop\Rust\data_type\target\debug\deps\data_type-8bf0d6db10293559.exe: src\main.rs
+
+C:\Users\fushe\Desktop\Rust\data_type\target\debug\deps\data_type-8bf0d6db10293559.d: src\main.rs
+
+src\main.rs:
