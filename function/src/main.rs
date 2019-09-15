083044c877afe6a2ed96bb87b258197f5b0c6775
Fu-Shengsheng <fushengsheng@live.com>
2019-09-15 23:15:16 +0800

    function-参数与返回值

--- function/src/main.rs
+++ function/src/main.rs
@@ -13,15 +13,28 @@ fn main() {
     another_function(99_999);
 
     some_func('A', 666);
-    
+
     // rust 的语句并不返回值，有别于C
     // 所以不能使用 let x = y = 1 的写法
-    let x= 6;
+    let x = 6;
 
     // rust 的表达式返回值
     // 函数是一个表达式
     // 宏调用是一个表达式
     // 创建新作用域的大括号（{}代码块）也是一个表达式
+    let y = {
+        let z = 3;
+        // 该表达式的值是 z+1 的值，即4，故y=4
+        // 表达式的结尾没有分号，加上分号就变成了语句
+        z + 1
+    };
+    println!("The value of y is: {}", y);
+
+    let m: i32 = five();
+    println!("The value of m is: {}", m);
+
+    let n = plus_one(m);
+    println!("The value of n is: {}", n);
 }
 
 fn another_function(value: i32) {
@@ -30,4 +43,15 @@ fn another_function(value: i32) {
 
 fn some_func(key: char, value: i32) {
     println!("Your key is :{} & value is :{}", key, value);
+}
+
+// 函数可以向调用它的代码返回值
+// 需要在 箭头（->） 后声明它的类型
+// rust 函数的返回值等同于函数体最后一个表达式的值
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
